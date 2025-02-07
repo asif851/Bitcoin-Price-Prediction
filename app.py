704e69b3d0f71fd24775a8d2675c1b0e7a6a2aed
asif851 <asifmredha21@gmail.com>
2025-02-07 21:20:37 +0600

    added code

--- app.py
+++ app.py
@@ -12,19 +12,15 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        Open = float(request.form['Open'])
+        Open=float(request.form['Open'])
         High = float(request.form['High'])
         Low = float(request.form['Low'])
         Volume = float(request.form['Volume'])
-        Month = int(request.form['Month'])
-        Day = int(request.form['Day'])
-        Year = int(request.form['Year'])
-        Daily_Return = float(request.form['Daily_Return'])
-        MA_7 = float(request.form['MA_7'])
-        MA_30 = float(request.form['MA_30'])
-        Volatility_30 = float(request.form['Volatility_30'])
+        MA_7day = float(request.form['7-day-MA'])
+       
 
-        data = np.array([[Open, High, Low, Volume, Month, Day, Year, Daily_Return, MA_7, MA_30, Volatility_30]])
+
+        data = np.array([[ Open,High, Low, Volume, MA_7day]])
         my_prediction = model.predict(data)
 
         return render_template('home.html', prediction=my_prediction)
